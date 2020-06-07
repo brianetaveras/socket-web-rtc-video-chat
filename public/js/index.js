fdb79ec79cbbabcc9155ab96dab199a9b1618f74
brianetaveras <taveras574@gmail.com>
2020-06-07 13:09:46 -0400

    Added click handler to individual users

--- public/js/index.js
+++ public/js/index.js
@@ -14,12 +14,31 @@ error=>{
 }
 )
 
+
+function callFriend(id){
+    console.log(id);
+}
+
+function addUserToList(user){
+    const li = document.createElement('li');
+    li.innerHTML = `${user.id}`
+
+    
+    li.addEventListener('click', () =>{
+        callFriend(user);
+    });
+
+    return li;
+}
+
 socket.on('updateUserList', (users) =>{
     user_list.innerHTML = ``;
     for(const user in users){
-        user_list.innerHTML += `
-            <li>${users[user].name}</li>
-        `
+        user_list.appendChild(addUserToList(users[user]));
+        
     }
-})
+});
+
+
+
 
