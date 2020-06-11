d1baf7be778c9034f2a232a6f8360da51264fb75
Joshua Burleson <josh.burleson@bytes.co>
2020-06-10 20:40:58 -0400

    File structure activated

--- thisfuckingvideoshit/src/vidz.jsx
+++ thisfuckingvideoshit/src/vidz.jsx
@@ -2,79 +2,8 @@ import React, {useState, useEffect, useRef} from 'react';
 import VideoCall from './VideoCall';
 import io from "socket.io-client";
 
-const Video = props => {
-    const [streamState, setStreamState] = useState({
-        stream: false,
-        active: false
-    });
-    
-    const videoRef = useRef();
-  
-    useEffect(() => { 
-      if( streamState.stream ){
-        videoRef.current.srcObject = streamState.stream;
-      }
-    }, [streamState.stream]);
-
-    useEffect(() => { 
-      if( props.stream ){
-        setStreamState({ stream: props.stream, active: true });
-      }
-    }, [props.stream]);
-
-    const startStream = async () => {
-        try {
-            const newStream = await navigator.mediaDevices.getUserMedia({audio: true, video: true});
-            setStreamState({stream: newStream, active: true});
-            props.setUserStream(newStream);
-        } catch(err){
-            console.error(err);
-        }
-    }
-
-    const stopStream = () => {
-        streamState.stream.getTracks().forEach(track => track.stop());
-        setStreamState({...streamState, active: false})
-    }
-
-    const handleClick = () => {
-        streamState.active ? stopStream() : startStream();
-    }
-
-    return (
-        <div className="stream-block">
-            <video style={{'background': 'black'}} ref={ videoRef } autoPlay playsInline muted controls></video>
-           { props.user === 'self' && <button onClick={ handleClick }>{ streamState.active ? 'Stop' : 'Start' } Video </button> }
-        </div>
-    );
-}
-
-
-const Caller = props => {
-    const handleAnswer = e => {
-        e.preventDefault();
-        props.answer( props.caller, props.answerCallback, props.userStream );
-    }
-    return(
-        <>
-            <button value={ props.caller.id } onClick={handleAnswer} >Answer</button>
-        </>
-    );
-}
-
-const Available = props => {
-    console.log( props.callPeer )
-    const handleCall = e => {
-        e.preventDefault();
-        props.callPeer( e.target.value, props.userStream ,props.setPartnerStream );
-    }
-    return (
-        <ul>
-            { props.available.map( user => user === props.userId ? null : <button key={ user } value={ user } onClick={ handleCall }>Call User</button> ) }
-        </ul>
-    );
-}
-
+import Video from './Video';
+import { Caller, Available } from './PeerConnections';
 
 const Chat = () => {
     const [chatSession, setChatSession] = useState({
@@ -97,34 +26,26 @@ const Chat = () => {
 
     const [ usersOnline, updateOnline ] = useState({})
     const [callController, setCallController] = useState(false);
-
     const socket = useRef();
-
     const setUserStream = stream => setChatSession({...chatSession, userStream: stream});
     const setPartnerStream = stream => setChatSession({...chatSession, partnerStream: stream});
 
     useEffect(() => {
-        socket.current = io.connect("https://65956de79733.ngrok.io/");
-    
+        socket.current = io.connect('https://8d1cfefbdbc8.ngrok.io/');
         socket.current.on("init", id => {
           setCallController( new VideoCall(id, socket) );
           setChatSession({...chatSession, userId: id });
         })
-
         socket.current.on("usersOnline", (users) => {
           updateOnline({...users});
         })
-    
         socket.current.on("callRequest", (data) => {
-            console.log('calling')
-            console.log(data);
           setCallData({...callData, caller: { 
               id: data.from, name: data.from, signal: data.signal 
             }
            });
         });
       }, []);
-
     return ( 
         <div>
             <Video user='self' setUserStream={setUserStream} />
